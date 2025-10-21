2cdea9f5e68a121489c56e8b4d1a279347c8a118
elijah-afriyie <elijah.afry@gmail.com>
2025-10-21 00:04:24 +0000

    Update

--- 11-ejs-and-partials/app.js
+++ 11-ejs-and-partials/app.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
 import { fileURLToPath } from 'url';
 import redditData from './data.json' with { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layout');
 
 app.get('/', (req, res) => {
-	res.render('home');
+	res.render('pages/home', { title: 'Home' });
 });
 
 app.get('/r/:subreddit', (req, res) => {
@@ -22,7 +27,7 @@ app.get('/r/:subreddit', (req, res) => {
 	if (!data) {
 		return res.status(404).render('notfound', { subreddit });
 	}
-	res.render('subreddit', { ...data });
+	res.render('pages/subreddit', { ...data });
 });
 
 export default app;
